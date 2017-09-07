4da9e4cc4e8c0fda81595c9337b5dee124d701e8
Luka <luka.pilipovic@gmail.com>
2017-09-08 00:44:09 +0200

    v0.9

--- XMLfolders.py
+++ XMLfolders.py
@@ -3,17 +3,21 @@ import os
 from xml.sax.saxutils import escape as xml_escape
 
 def DirAsXML(path):
-    result = '<folder>\n<name>%s</name>\n' % xml_escape(os.path.basename(path))
+    if (path[-3:] == 'svg'
+            or path == 'icon'):
+        result = ''
+    else:
+       result = ''
+
+
     dirs = []
     files = []
     for item in os.listdir(path):
         itempath = os.path.join(path, item)
         if os.path.isdir(itempath):
 
-
             if (item[5:6] == '-' or
                     item == 'svg'):
-                #print(item[6:])
                 dirs.append(item)
             else:
                 continue
@@ -23,15 +27,21 @@ def DirAsXML(path):
             else:
                 continue
     if files:
-        result += '  <files>\n' \
-            + '\n'.join('    <file>\n      %s\n    </file>'
-            % xml_escape(f) for f in files) + '\n  </files>\n'
+        result += ' <files>\n' \
+            + '\n'.join('    <file>\n       %s\n    </file>'
+            % xml_escape(f) for f in files) + '\n</files>\n'
     if dirs:
         for d in dirs:
-            # if d != 'svg':
-            x = DirAsXML(os.path.join(path, d))
-            result += '\n'.join('  ' + line for line in x.split('\n'))
-        result += '</folder>'
+
+            if d == 'svg':
+                x = DirAsXML(os.path.join(path, d))
+                result += '\n'.join('  ' + line for line in x.split('\n'))
+
+            else:
+                result += '<folder>\n<name>' + d + '</name>\n'
+                x = DirAsXML(os.path.join(path, d))
+                result += '\n'.join(line for line in x.split('\n'))
+                result += '</folder>\n'
     return result
 
 if __name__ == '__main__':
