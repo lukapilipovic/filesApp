1535a687823b37732aff2c8f3a8d74ef1bfa3ed7
Luka Pilipovic <lukapilipovic@codija.lan>
2017-09-08 17:28:37 +0200

    loaduje slicice

---
+++ foldersToJSON.py
@@ -0,0 +1,49 @@
+
+import os
+from xml.sax.saxutils import escape as xml_escape
+
+def DirAsJSON(path):
+
+    result = ''
+    dirs = []
+    files = []
+    for item in os.listdir(path):
+        itempath = os.path.join(path, item)
+        if os.path.isdir(itempath):
+
+            if (item[6:7] == '-' or
+                    item == 'svg'):
+                dirs.append(item)
+            else:
+                continue
+        elif os.path.isfile(itempath):
+            if item[-3:] == 'svg':
+                files.append(item)
+            else:
+                continue
+    if files:
+        result += '\n \"folderElements\" : ['
+        for x in files:
+            result += '\"%s\",' % (x)
+        result += ']'
+
+    if dirs:
+        for d in dirs:
+            if d == 'svg':
+                x = DirAsJSON(os.path.join(path, d))
+                for line in x.split('\n'):
+                    result += '\n' + line + '\n'
+            else:
+                result += '{\n\"folderName\": \"' + d[7:] + '\",\n'
+                result += '\"folderUrl\": \"' + path + '/' + d + '\",\n'
+                x = DirAsJSON(os.path.join(path, d))
+                result += ''.join(line for line in x.split('\n'))
+                result += '\n}\n'
+    return result
+
+if __name__ == '__main__':
+
+    print (DirAsJSON('IconsDesign'))
+    # f = open('map.json', 'w')
+    # output = DirAsXML('IconsDesign')
+    # f.write(output)
