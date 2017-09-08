da35efc6b02d490988e487246247cd061e2b6109
Luka Pilipovic <lukapilipovic@codija.lan>
2017-09-08 10:17:17 +0200

    sa XMLom

--- XMLfolders.py
+++ XMLfolders.py
@@ -16,13 +16,13 @@ def DirAsXML(path):
         itempath = os.path.join(path, item)
         if os.path.isdir(itempath):
 
-            if (item[5:6] == '-' or
+            if (item[6:7] == '-' or
                     item == 'svg'):
                 dirs.append(item)
             else:
                 continue
         elif os.path.isfile(itempath):
-            if item[-4:] == 'jpeg':
+            if item[-3:] == 'svg':
                 files.append(item)
             else:
                 continue
@@ -32,17 +32,17 @@ def DirAsXML(path):
             % xml_escape(f) for f in files) + '\n</files>\n'
     if dirs:
         for d in dirs:
-
             if d == 'svg':
                 x = DirAsXML(os.path.join(path, d))
                 result += '\n'.join('  ' + line for line in x.split('\n'))
-
             else:
-                result += '<folder>\n<name>' + d + '</name>\n'
+                result += '<folder>\n<name>' + d[7:] + '</name>\n'
                 x = DirAsXML(os.path.join(path, d))
                 result += '\n'.join(line for line in x.split('\n'))
                 result += '</folder>\n'
     return result
 
 if __name__ == '__main__':
-    print  DirAsXML('icons')
+    f = open('map.xml', 'w')
+    output = DirAsXML('IconsDesign')
+    f.write(output)
